fb180c494dff2a27ad6a2738e6eac1a073d85891
Prince Codes <princecodes247@gmail.com>
2022-02-25 16:51:00 +0100

    feat: create settings page

---
+++ src/app/pages/settings/settings.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/user.service';
+import { MenuController, ToastController } from '@ionic/angular';
+import { AccountService } from '../../shared/account.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+})
+export class SettingsPage implements OnInit {
+ 
+
+
+  
+  ngOnInit() {
+  }
+ 
+
+
+
+}
