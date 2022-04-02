c34b16643aa2c95ffd84a3f327c05eed15e59099
Prince Codes <princecodes247@gmail.com>
2022-04-02 16:41:13 +0100

    feat: create profile pic component

---
+++ src/app/components/profile-photo/profile-photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile-photo',
+  templateUrl: './profile-photo.component.html',
+  styleUrls: ['./profile-photo.component.scss'],
+})
+export class ProfilePhotoComponent implements OnInit {
+ 
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
