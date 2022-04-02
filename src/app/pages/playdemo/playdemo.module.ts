8c37ee3bc243f0cb9e61ca84379587d1b6eeb864
Prince Codes <princecodes247@gmail.com>
2022-04-02 16:42:10 +0100

    feat: update play demo page

--- src/app/pages/playdemo/playdemo.module.ts
+++ src/app/pages/playdemo/playdemo.module.ts
@@ -5,9 +5,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
+import { LoadingSpinnerComponent } from 'src/app/components/loading-spinner/loading-spinner.component';
 import { PlaydemoPage } from './playdemo.page';
 import { CountdownTimerComponent } from 'src/app/components/countdown-timer/countdown-timer.component';
 import { ClueTipsComponent } from '../../components/clue-tips/clue-tips.component';
+import { ScoreCounterComponent } from 'src/app/components/score-counter/score-counter.component';
 
 const routes: Routes = [
   {
@@ -23,6 +25,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [PlaydemoPage, CountdownTimerComponent, ClueTipsComponent]
+  declarations: [PlaydemoPage, CountdownTimerComponent, ClueTipsComponent, ScoreCounterComponent, LoadingSpinnerComponent]
 })
 export class PlaydemoPageModule {}
