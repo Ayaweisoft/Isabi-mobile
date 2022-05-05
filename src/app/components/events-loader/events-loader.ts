2ce940740165866c2e7bcda465a0ce6dc85aad25
Prince Codes <princecodes247@gmail.com>
2022-05-05 03:12:59 -0400

    update events loader

---
+++ src/app/components/events-loader/events-loader.ts
@@ -0,0 +1,22 @@
+import { IonSlides } from '@ionic/angular';
+import { Component, ViewChild, OnInit } from '@angular/core';
+@Component({
+  selector: 'events-loader',
+  templateUrl: './events-loader.component.html',
+  styleUrls: ['./events-loader.component.scss'],
+})
+export class ClueTipsComponent implements OnInit {
+
+
+  constructor(
+    
+  ) { }
+
+  ngOnInit() {
+
+  }
+
+
+
+
+}
