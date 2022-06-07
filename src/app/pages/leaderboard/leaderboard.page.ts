7ab5b8c8dfe39b39121558facce995ac6899b8a5
Prince Codes <princecodes247@gmail.com>
2022-06-07 02:11:07 -0400

    fix: minutes in leaderboard not showing

--- src/app/pages/leaderboard/leaderboard.page.ts
+++ src/app/pages/leaderboard/leaderboard.page.ts
@@ -111,6 +111,18 @@ export class LeaderboardPage implements OnInit {
     this.loading = true;
     this.accountServive.getLeaderboard().subscribe(val => {
       this.leaderBoard = val["document"];
+      this.leaderBoard = this.leaderBoard.map(player => {
+        if (!player.time) {
+            player.time = 0;
+        }
+        if (player.time > 60) {
+          
+          player.time = `${Math.floor(player.time / 60)} mins ${Math.floor(player.time % 60)} secs`
+        } else {
+          player.time = player.time + " secs"
+        }
+        return player;
+      })
       this.firstPerson = this.leaderBoard[0];
       this.secondPerson = this.leaderBoard[1];
       this.thirdPerson = this.leaderBoard[2];
